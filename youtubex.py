2f0dac48e24784bc25ab7f2008e7bd4d370a774c
natgho <admin@sezerbozkir.com>
2019-11-02 20:15:20 +0300

    get parameter from prompt added.

--- youtubex.py
+++ youtubex.py
@@ -5,19 +5,18 @@ import sys
 import os
 from utils import download_from_link
 
-
 if __name__ == '__main__':
     if os.name == "nt":
         saving_path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+        link = input("Please enter youtube link?")
+        output_path = input("Please enter output path?")
     else:
         saving_path = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop')
-
-    # link = "https://www.youtube.com/watch?v=xWOoBJUqlbI"
-    # download_from_link(link)
-    if len(sys.argv) != 3:
-        print(
-            "Using: python youtubex.py <youtube_link> <output_path>")
-        sys.exit(0)
-
-    link, output_path = sys.argv[1:3]
+        if len(sys.argv) != 3:
+            print(
+                "Using: python youtubex.py <youtube_link> <output_path>")
+            sys.exit(0)
+        link, output_path = sys.argv[1:3]
+    # sample link = https://www.youtube.com/watch?v=xWOoBJUqlbI
+    # sample path = C:\Users\<user_name>\Desktop
     download_from_link(link, output_path)
